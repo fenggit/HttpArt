382c676ffca0dad9084549a1e05b12a03c185007
hefeng <315889714@qq.com>
2020-02-07 20:54:07 +0800

    simple

--- simple-server/server.js
+++ simple-server/server.js
@@ -1,10 +1,9 @@
-const http  =require('http');
-const fs = require('fs');
+const http = require('http');
 
-http.createServer(function (request,response) {
-    console.log("request url:",request.url);
+http.createServer(function (request, response) {
+    console.log("request url:", request.url);
     response.end("success");
 }).listen(8080);
 
 console.log("server listening on 8080");
-console.log("http://localhost:8080/");
+console.log("http://localhost:8080/ or http://127.0.0.1:8080/");
