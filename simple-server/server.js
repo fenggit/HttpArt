6cb93c54a6b23b05000b12655484ef2755ea920d
hefeng <315889714@qq.com>
2020-02-10 15:28:26 +0800

    update server

--- simple-server/server.js
+++ simple-server/server.js
@@ -2,8 +2,15 @@ const http = require('http');
 
 http.createServer(function (request, response) {
     console.log("request url:", request.url);
-    response.end("success");
-}).listen(8080);
 
-console.log("server listening on 8080");
-console.log("http://localhost:8080/ or http://127.0.0.1:8080/");
+    response.writeHead(200,{
+        // 关闭长连接
+        'Connection':'close'
+        // HTTP/1.1 默认开启长连接：Connection: keep-alive
+        //'Connection':'keep-alive'
+    });
+    response.end("success");
+}).listen(8081);
+
+console.log("server listening on 8081");
+console.log("http://localhost:8081/ or http://127.0.0.1:8081/");
